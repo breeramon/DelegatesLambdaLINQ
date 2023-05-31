aad617ecdf1a4fe232e7cbbbdcf584727fe4daa2
Breno Ramon <breeramon@gmail.com>
2023-05-31 09:25:23 -0300

    Função Predicate, usado com RemoveAll

--- DelegatesLambdaLINQ/Program.cs
+++ DelegatesLambdaLINQ/Program.cs
@@ -4,23 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DelegatesLambdaLINQ.Services;
+using DelegatesLambdaLINQ.Entities;
+using System.Globalization;
 
 namespace DelegatesLambdaLINQ
 {
-    delegate void BinaryNumericOperation(double n1, double n2);
     class Program
     {
         static void Main(string[] args)
         {
-            double a = 10;
-            double b = 12;
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
 
-            BinaryNumericOperation op = CalculationService.ShowSum;
-            op += CalculationService.ShowMax;
+            //Uso sem a expressão lambda no list.RemoveAll()
+            list.RemoveAll(ProductTest);
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
 
-            op(a, b);
+            //Uso com a expressão lambda no list.RemoveAll()
+            list.RemoveAll(p2 => p2.Price <= 100);
+            foreach (Product p2 in list)
+            {
+                Console.WriteLine(p2);
+            }
+        }
 
-
+        public static bool ProductTest(Product p)
+        {
+            return p.Price >= 100;
         }
     }
 }
