584eefea980b030a9784fff3ac31cbd83834ab2c
Breno Ramon <breeramon@gmail.com>
2023-05-31 09:03:57 -0300

    Multicast delegates

---
+++ DelegatesLambdaLINQ/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DelegatesLambdaLINQ.Services;
+
+namespace DelegatesLambdaLINQ
+{
+    delegate void BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            op(a, b);
+
+
+        }
+    }
+}
