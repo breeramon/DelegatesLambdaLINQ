507e2c7b502dcf6b38927c08a8a1b851e4176046
Breno Ramon <breeramon@gmail.com>
2023-06-01 09:22:29 -0300

    LINQ com notação similar ao SQL

--- DelegatesLambdaLINQ/Program.cs
+++ DelegatesLambdaLINQ/Program.cs
@@ -40,19 +40,39 @@ namespace DelegatesLambdaLINQ
                 new Product(11, "Level", 70.0, c1)
             };
 
-            var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900);
+            //var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900); === Modo de escrita usando apenas o LINQ
+            var r1 = // LINQ com notação(modo de escrita) similar ao SQL, no exemplo abaixo
+                from p in products
+                where p.Category.Id == 1 && p.Price < 900
+                select p;
             Print("TIER 1 AND PRICE < 900", r1);
 
-            var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            //var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            var r2 =
+                from p in products
+                where p.Category.Name == "Tools"
+                select p.Name;
             Print("NAMES OF PRODUCTS FROM TOOLS", r2);
 
-            var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name});
+            //var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name});
+            var r3 =
+                from p in products
+                where p.Name[0] == 'C'
+                select new { p.Name , p.Price, CategoryName = p.Category.Name};
             Print("NAMES STARTED WITH 'C' AND ANONYMOUS OBJECT", r3);
 
-            var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            //var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            var r4 =
+                from p in products
+                where p.Category.Tier == 1
+                orderby p.Price, p.Name
+                select p;
             Print("TIER 1 ORDER BY PRICE THEN BY NAME", r4);
 
-            var r5 = r4.Skip(2).Take(4);
+            //var r5 = r4.Skip(2).Take(4);
+            var r5 = 
+                (from p in r4
+                 select p).Skip(2).Take(4);
             Print("TIER 1 ORDER BY PRICE THEN BY NAME SKIP 2 TAKE 4", r5);
 
             var r6 = products.FirstOrDefault();
@@ -86,7 +106,9 @@ namespace DelegatesLambdaLINQ
             Console.WriteLine("Category 1 aggregate sum: {0}", r15);
             Console.WriteLine();
 
-            var r16 = products.GroupBy(p => p.Category);
+            //var r16 = products.GroupBy(p => p.Category);
+            var r16 = from p in products
+                      group p by p.Category;
             foreach (IGrouping<Category, Product> group in r16)
             {
                 Console.WriteLine("Category " + group.Key.Name + ":");
